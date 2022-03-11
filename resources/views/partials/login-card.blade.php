dc80197ffda48f4438618425d741fb2e1819356f
ajcastro <ajcastro29@gmail.com>
2022-03-11 20:58:34 +0800

    Fix login

--- resources/views/partials/login-card.blade.php
+++ resources/views/partials/login-card.blade.php
@@ -1,4 +1,6 @@
 <div class="card login-card card-main">
+<form method="POST" action="{{ url('login') }}">
+    @csrf
     <div class="card-body">
         <div class="mb-3 text-center text-uppercase">
             <h5>
@@ -7,16 +9,17 @@
         </div>
         <div class="mb-3">
             <label for="username-form-control" class="form-label">Username</label>
-            <input type="email" class="form-control" id="username-form-control" placeholder="">
+            <input type="text" class="form-control" id="username-form-control" placeholder="">
         </div>
         <div class="mb-3">
             <label for="password-form-control" class="form-label">Password</label>
-            <input type="email" class="form-control" id="password-form-control" placeholder="">
+            <input type="password" class="form-control" id="password-form-control" placeholder="">
         </div>
         <div class="mb-2">
-            {{-- <button type="submit" class="btn btn-primary">Login</button> --}}
-            <a href="{{url('/login')}}" class="btn btn-primary">Login</a>
+            <button type="submit" class="btn btn-primary">Login</button>
+            {{-- <a href="{{url('/login')}}" class="btn btn-primary">Login</a> --}}
             <a href="{{url('/register')}}" class="btn btn-link float-end">Register</a>
         </div>
     </div>
+</form>
 </div>

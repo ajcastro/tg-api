d08cfb56f8605adb1021dbddc7789fe8f31f8076
ajcastro <ajcastro29@gmail.com>
2022-03-16 16:25:11 +0800

    Fix Website scopeAccessibleBy()

--- app/Models/Website.php
+++ app/Models/Website.php
@@ -91,6 +91,10 @@ class Website extends Model implements AccessibleByUser
 
     public function scopeAccessibleBy($query, User $user)
     {
+        if ($user->isSuperAdmin()) {
+            return;
+        }
+
         $query->where('assigned_client_id', $user->getCurrentClient()->id);
     }
 }

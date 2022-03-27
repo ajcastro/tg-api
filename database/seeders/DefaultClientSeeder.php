a55c6d6db6cc74ba4bbc3ff1b7fdb9758f60d279
ajcastro <ajcastro29@gmail.com>
2022-03-27 15:53:03 +0800

    Fix

--- database/seeders/DefaultClientSeeder.php
+++ database/seeders/DefaultClientSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Client;
+use App\Models\User;
+use App\Models\Website;
 use Illuminate\Database\Seeder;
 
 /** @see Client::createDefauParentGroupAndAdministratorUser() */
@@ -15,13 +17,30 @@ class DefaultClientSeeder extends Seeder
      */
     public function run()
     {
-        Client::firstOrCreate([
+        /** @var Client */
+        $client = Client::firstOrCreate([
             'id' => Client::DEFAULT_ID,
             'code' => Client::DEFAULT_CODE,
         ], [
             'remarks' => 'Default client',
             'is_active' => true,
             'is_hidden' => true,
+        ]);
+
+        $this->seedDefaultWebsite($client);
+    }
+
+    public function seedDefaultWebsite($client)
+    {
+        Website::firstOrCreate([
+            'assigned_client_id' => $client->id,
+            'code' => 'tg',
+        ], [
+            'ip_address' => '127.0.0.1',
+            'domain_name' => 'telegaming.net',
+            'is_active' => true,
+            'created_by_id' => User::ADMIN_ID,
+            'updated_by_id' => User::ADMIN_ID,
         ]);
     }
 }

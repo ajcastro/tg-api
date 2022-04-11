44c550ebc8821c262c8deba87eedf831b100f9a7
ajcastro <ajcastro29@gmail.com>
2022-04-11 16:59:02 +0800

    Add UpdateAdministratorPermissions

---
+++ database/seeders/UpdateAdministratorPermissions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+
+class UpdateAdministratorPermissions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::truncate();
+        $this->call(PermissionsTableSeeder::class);
+        $roles = Role::where('name', 'Administrator')->get();
+
+        $roles->each->assignAllPermissions();
+    }
+}

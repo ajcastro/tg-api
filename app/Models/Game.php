23df3ceb1872b2535dd340e2c573dc312731f08c
ajcastro <ajcastro29@gmail.com>
2022-04-15 14:34:05 +0800

    Add Game, GameCategory, Rebate models and migrations

---
+++ app/Models/Game.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'game_category_id',
+        'title',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'game_category_id' => 'integer',
+    ];
+
+    public function gameCategory()
+    {
+        return $this->belongsTo(GameCategory::class);
+    }
+}

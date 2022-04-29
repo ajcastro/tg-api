c8db26a13e6859532d2a07abef9c5a2bd63ad431
ajcastro <ajcastro29@gmail.com>
2022-04-29 14:30:55 +0800

    Member's UserLog

--- app/Http/Controllers/Traits/GetsRecord.php
+++ app/Http/Controllers/Traits/GetsRecord.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait GetsRecord
 {
     public function getRecord($id)
     {
+        if ($id instanceof Model) {
+            return $id;
+        }
+
         // TODO: perform caching here that will be reset on every after http requests
         return $this->model()->resolveRouteBinding($id);
     }

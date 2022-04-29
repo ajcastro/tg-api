06eaffc8449d8618a68438494a138e1e01c60671
ajcastro <ajcastro29@gmail.com>
2022-04-29 10:41:31 +0800

    Refactor CrudUserLog

--- app/Http/Controllers/Traits/CrudUserLog.php
+++ app/Http/Controllers/Traits/CrudUserLog.php
@@ -14,26 +14,38 @@ trait CrudUserLog
     protected function setUpCrudUserLog()
     {
         $this->hook(function (Request $request) {
-            $this->makeUserLog($request, $this->crudUserLog->index($request))->save();
+            $this->makeUserLog($request)
+                ->fill($this->crudUserLog->index($request))
+                ->save();
         })->only(['index']);
 
         $this->hook(function (Request $request) {
-            $this->makeUserLog($request, $this->crudUserLog->show($request, $this->resolveRecord($request)))->save();
+            $this->makeUserLog($request)
+                ->fill($this->crudUserLog->show($request, $this->resolveRecord($request)))
+                ->save();
         })->only(['show']);
 
         $this->hook(function (Request $request) {
-            $this->makeUserLog($request, $this->crudUserLog->store($request))->save();
+            $this->makeUserLog($request)
+                ->fill($this->crudUserLog->store($request))
+                ->save();
         })->only(['store']);
 
         $this->hook(function (Request $request) {
-            $this->makeUserLog($request, $this->crudUserLog->update($request, $this->resolveRecord($request)))->save();
+            $this->makeUserLog($request)
+                ->fill($this->crudUserLog->update($request, $this->resolveRecord($request)))
+                ->save();
         })->only(['update']);
 
         $this->hook(function (Request $request) {
             if ($request->boolean('is_active')) {
-                $this->makeUserLog($request, $this->crudUserLog->activate($request, $this->resolveRecord($request)))->save();
+                $this->makeUserLog($request)
+                    ->fill($this->crudUserLog->activate($request, $this->resolveRecord($request)))
+                    ->save();
             } else {
-                $this->makeUserLog($request, $this->crudUserLog->deactivate($request, $this->resolveRecord($request)))->save();
+                $this->makeUserLog($request)
+                    ->fill($this->crudUserLog->deactivate($request, $this->resolveRecord($request)))
+                    ->save();
             }
         })->only(['setActiveStatus']);
     }
@@ -43,25 +55,8 @@ trait CrudUserLog
         throw new Exception('Missing method resolveRecord() in '.get_class($this));
     }
 
-    protected function makeUserLog(Request $request, array $userLogAttributes): UserLog
+    protected function makeUserLog(Request $request): UserLog
     {
-        return UserLog::make($userLogAttributes + [
-            'website_id' => $this->resolveWebsiteIdForUserLog($request),
-            'user_id' => $request->user()->id,
-            'date' => now(),
-            'user_ip' => $request->ip(),
-            'user_info' => agent_user_info(),
-            'member_id' => $this->resolveMemberIdForUserLog($request),
-        ]);
-    }
-
-    protected function resolveWebsiteIdForUserLog(Request $request)
-    {
-        return $request->input('website_selector_website_id');
-    }
-
-    protected function resolveMemberIdForUserLog(Request $request)
-    {
-        return $request->input('member_id');
+        return UserLog::fromRequest($request);
     }
 }

c53f14abc6f15ff13e24d21b70eea2610ce0841e
ajcastro <ajcastro29@gmail.com>
2022-04-30 18:19:26 +0800

    Add other permissions for dashboard cards

--- database/seeders/permissions/dashboard.php
+++ database/seeders/permissions/dashboard.php
@@ -11,4 +11,31 @@ return [
             'subject' => 'Dashboard',
         ],
     ],
+    [
+        'name' => 'dashboard.member_registration_brief',
+        'label' => 'Dashboard - Member Registration Brief',
+        'admin_redirect' => '/dashboard',
+        'casl' => [
+            'action' => 'view',
+            'subject' => 'MemberRegistrationBrief',
+        ],
+    ],
+    [
+        'name' => 'dashboard.revenue_and_new_registration_performance',
+        'label' => 'Dashboard - Revenue and New Registration Performance',
+        'admin_redirect' => '/dashboard',
+        'casl' => [
+            'action' => 'view',
+            'subject' => 'RevenueAndNewRegistrationPerformance',
+        ],
+    ],
+    [
+        'name' => 'dashboard.website_revenue',
+        'label' => 'Dashboard - Website Revenue',
+        'admin_redirect' => '/dashboard',
+        'casl' => [
+            'action' => 'view',
+            'subject' => 'WebsiteRevenue',
+        ],
+    ],
 ];

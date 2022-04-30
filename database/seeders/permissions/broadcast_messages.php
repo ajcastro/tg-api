bc04c02b357a5b330fe433c3445272ed6e28fac5
ajcastro <ajcastro29@gmail.com>
2022-04-30 18:40:26 +0800

    Fix

--- database/seeders/permissions/broadcast_messages.php
+++ database/seeders/permissions/broadcast_messages.php
@@ -5,7 +5,7 @@ return [
     [
         'name' => 'broadcast_message',
         'label' => 'Action - Broadcast Message',
-        'admin_redirect' => '/',
+        'admin_redirect' => '/dashboard',
         'casl' => [
             'action' => 'broadcast',
             'subject' => 'BroadcastMessage',

677cacd3e9b6c2d337f7c8bd80aca519b566abbf
ajcastro <ajcastro29@gmail.com>
2022-05-17 14:20:51 +0800

    Add todo comment

--- app/Models/Market.php
+++ app/Models/Market.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * TODO: Sync markets table from game4d
+ */
 class Market extends Model
 {
     use HasFactory, Traits\HasAllowableFields;
